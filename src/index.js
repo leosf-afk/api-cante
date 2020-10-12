b94a0464850b4bcac5c497887c6ca25375fbab25
leo <leo1104_tuc@hotmail.com>
2020-10-12 05:35:27 -0300

    api terminada

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 consign({
   cwd: __dirname
 })
+//ejecuta la config de base de datos, la base y luego middlewares para configurar el sv
+//luego inicia el sv
 .include('libs/config.js')
 .then('db.js')
 .then('libs/middlewares.js')
--- src/libs/boot.js
+++ src/libs/boot.js
@@ -1,7 +1,30 @@
 //arranca el servidor
 
-module.exports = app =>{
+module.exports = app => {
+
+(async() => await app.db.sequelize.sync({alter:true}))();
+
   app.listen(app.get('port'), () => {
-    console.log('server on port', app.get('port'));
+    console.log('Server on port', app.get('port'));
   });
+
+
 };
+
+
+
+
+
+/** app.db.sequelize.sync().done(() => {
+    app.listen(app.get('port'), () => {
+      console.log('Server on port', app.get('port'));
+    });
+  });
+  **/
+
+
+  /**
+   async function test () {
+    await app.db.sequelize.sync();
+  }
+  **/
